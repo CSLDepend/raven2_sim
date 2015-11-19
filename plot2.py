150e7ef0b6edc24c6d753cafc84a074a4db299a0
Homa Alemzadeh alemzad1@illinois.edu <shawnlee824@gmail.com>
2015-11-18 20:04:35 -0600

    Added the number of packets to the log file

--- plot2.py
+++ plot2.py
@@ -80,7 +80,7 @@ if mode == "log":
 		csvfile4 = open(raven_home+'/fault_free_log.csv','w')
 		writer4 = csv.writer(csvfile4,delimiter=',') 
 		# For rt_process_preempt:
-		output_line = ''
+		output_line = 'Num_Packets'
 		for i in range(0,len(mpos)):
 			output_line = output_line + 'err_mpos' + str(indices[i]) + ','
 			output_line = output_line + 'err_mvel' + str(indices[i]) + ','
@@ -96,20 +96,19 @@ if mode == "log":
 	csvfile4 = open(raven_home+'/fault_free_log.csv','a')
 	writer4 = csv.writer(csvfile4,delimiter=',') 
 
-	output_line = ''
 	# Trajectory errors 
 	mpos_error = [];
 	mvel_error = [];
 	jpos_error = [];
 	pos_error = [];
-	for i in range(0,len(mpos)):
-		traj_len = min(len(mpos[0]),len(gold_mpos[0]))
+	output_line = str(len(mpos[0])) + ','
+	traj_len = min(len(mpos[0]),len(gold_mpos[0]))
+	for i in range(0,len(mpos)):		
 		mpos_error.append(float(sum(abs(np.array(mpos[i][1:traj_len])-np.array(gold_mpos[i][1:traj_len]))))/traj_len)
 		mvel_error.append(float(sum(abs(np.array(mvel[i][1:traj_len])-np.array(gold_mvel[i][1:traj_len]))))/traj_len)
 		jpos_error.append(float(sum(abs(np.array(jpos[i][1:traj_len])-np.array(gold_jpos[i][1:traj_len]))))/traj_len)
 		output_line = output_line + str(mpos_error[i]) + ', '+ str(mvel_error[i]) +', '+ str(jpos_error[i])+',' 
 	for i in range(0,len(pos)):    
-		traj_len = min(len(pos[0]),len(gold_pos[0]))
 		pos_error.append(float(sum(abs(np.array(pos[i][1:traj_len])-np.array(gold_pos[i][1:traj_len]))))/traj_len)
 		if (i == len(pos)-1):
 			output_line = output_line + str(pos_error[i])
--- run.py
+++ run.py
@@ -35,7 +35,7 @@ import csv
 import matplotlib.pyplot as plt
 import math
 from parse_plot import * 
-from time import time
+import time
 
 def rsp_func():
     """ Get response from user to check if raven_home directory is correct"""
@@ -380,7 +380,7 @@ class Raven():
             csvfile4 = open(self.raven_home+'/error_log.csv','w')
             writer4 = csv.writer(csvfile4,delimiter=',') 
             # For rt_process_preempt:
-            output_line = 'Variable, Start, Duration, Value, Errors, '
+            output_line = 'Variable, Start, Duration, Value, Num_Packets ,Errors, '
             for i in range(0,len(mpos)):
                 output_line = output_line + 'err_mpos' + str(indices[i]) + ','
                 output_line = output_line + 'err_mvel' + str(indices[i]) + ','
@@ -414,7 +414,8 @@ class Raven():
         param_line = [var,start, duration, val]'''
         print param_line
 
-        output_line = ''
+        # Len of Trajectory
+        output_line = str(len(mpos[0]))+','
         # Error messages
         gold_msgs = [s for s in gold_err if s]
         err_msgs = [s for s in err if s]
@@ -429,14 +430,13 @@ class Raven():
         mvel_error = [];
         jpos_error = [];
         pos_error = [];
-        for i in range(0,len(mpos)):
-		    traj_len = min(len(mpos[0]),len(gold_mpos[0]))
+        traj_len = min(len(mpos[0]),len(gold_mpos[0]))        
+        for i in range(0,len(mpos)):		    
 		    mpos_error.append(float(sum(abs(np.array(mpos[i][1:traj_len])-np.array(gold_mpos[i][1:traj_len]))))/traj_len)
 		    mvel_error.append(float(sum(abs(np.array(mvel[i][1:traj_len])-np.array(gold_mvel[i][1:traj_len]))))/traj_len)
 		    jpos_error.append(float(sum(abs(np.array(jpos[i][1:traj_len])-np.array(gold_jpos[i][1:traj_len]))))/traj_len)
 		    output_line = output_line + str(mpos_error[i]) + ', '+ str(mvel_error[i]) +', '+ str(jpos_error[i])+',' 
         for i in range(0,len(pos)):    
-            traj_len = min(len(pos[0]),len(gold_pos[0]))
             pos_error.append(float(sum(abs(np.array(pos[i][1:traj_len])-np.array(gold_pos[i][1:traj_len]))))/traj_len)
             if (i == len(pos)-1):
                 output_line = output_line + str(pos_error[i])
@@ -555,13 +555,15 @@ class Raven():
         else:
             self._compile_raven()
             s = 0
-            for i in range(0,100):
-                t0 = time()
+            n = 1
+            for i in range(0,n):
+                t0 = time.time()
                 self._run_experiment()
                 os.system('python '+raven_home+'/plot2.py log')
-                t1 = time()
-                s = s + (t1-t0)
-            print "Average run time = " + str(s/200) + " seconds\n"
+                t1 = time.time()
+                s = s + float(t1-t0)
+                print s
+            print "Average experiment run time = " + str(float(s/n)) + " seconds\n"
 
 # Main code starts here
 
